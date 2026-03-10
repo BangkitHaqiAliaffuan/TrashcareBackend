b03d6243dcdbfa38ef9828e9e5261823f8e26973
BangkitHaqiAliaffuan <uwawuwuz@gmail.com>
2026-03-10 14:13:57 +0700

    fix: remove AuthenticateSession from Filament middleware, add POST debug route

--- myapplicationbe/routes/web.php
+++ myapplicationbe/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Temporary debug: test if POST works at all
+Route::post('/debug-post', function () {
+    return response()->json(['status' => 'POST works', 'session' => session()->getId()]);
+});
